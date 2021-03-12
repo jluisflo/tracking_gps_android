bc944ee697e17365bc2eec2eba586231d3db8f7b
Luis <josefloresv.dev@gmail.com>
2021-03-11 23:47:00 -0600

    Send placa and btn destroy

--- app/src/main/java/com/app/tracking/AlarmService.java
+++ app/src/main/java/com/app/tracking/AlarmService.java
@@ -2,10 +2,12 @@ package com.app.tracking;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Process;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -16,11 +18,15 @@ import com.android.volley.toolbox.Volley;
 
 public class AlarmService extends Service {
 
+    String placa;
+
     private GpsTracker gpsTracker = new GpsTracker(this);
     RequestQueue queue;
 
-    private String API_API = "http://200.31.171.50";
-    private String API_ENDPOINT = "/insert_position.php";
+    //    private String API_API = "http://200.31.171.50/sistema2021";
+    private String API_API = "http:///192.168.1.100/tracking";
+    //private String API_ENDPOINT = "/insert_position.php";
+    private String API_ENDPOINT = "/index.php";
 
     @Override
     public void onCreate() {
@@ -30,23 +36,25 @@ public class AlarmService extends Service {
         Log.d("onCreate()", "After service created");
 
         queue = Volley.newRequestQueue(this);
+
+        placa = getPlaca();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
         //HERE PASS USER ASOCIATE TO PHONE
-        sendTracking("USUARIO QUEMADO");
+        sendTracking();
         Toast.makeText(getApplicationContext(), "Actualizando posicion", Toast.LENGTH_SHORT).show();
 
         return START_STICKY;
     }
 
-    private void sendTracking(String user) {
+    private void sendTracking() {
 
         Location location = getPosition();
 
-        String params = "?user=" + user + "&latitud=" + location.getLatitude() + "&longitud=" + location.getLongitude();
+        String params = "?placa=" + placa + "&latitud=" + location.getLatitude() + "&longitud=" + location.getLongitude();
         String url = API_API + API_ENDPOINT + params;
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -56,6 +64,12 @@ public class AlarmService extends Service {
                 }
         );
         queue.add(stringRequest);
+    }
+
+    public String getPlaca() {
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        return settings.getString("placa", "");
     }
 
     private Location getPosition() {
--- app/src/main/java/com/app/tracking/MainActivity.java
+++ app/src/main/java/com/app/tracking/MainActivity.java
@@ -1,9 +1,14 @@
 package com.app.tracking;
 
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -12,24 +17,67 @@ import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText txtPlaca;
+    Button btnTracking;
+    Button btnAbort;
+
+    AlarmReceiver alarm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        txtPlaca = findViewById(R.id.txtPlaca);
+        btnTracking = findViewById(R.id.btnTracking);
+        btnAbort = findViewById(R.id.btnAbort);
 
-        //CALLING SERVICE
-        AlarmReceiver alarm = new AlarmReceiver();
-        alarm.setAlarm(this);
+        // manage service
+        btnTracking.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                initService();
+            }
+        });
+
+        btnAbort.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                destroyService();
+            }
+        });
 
         try {
-            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void initService() {
+        if (saveUser(txtPlaca.getText().toString())) {
+            //CALLING SERVICE
+            alarm = new AlarmReceiver();
+            alarm.setAlarm(this);
+
+        }
+    }
+
+    public void destroyService(){
+        alarm.abortBroadcast();
+    }
+
+    boolean saveUser(String placa) {
+
+        if (placa.isEmpty()) return false;
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("placa", placa);
+        editor.commit();
+
+        return true;
     }
 
     @Override
